97e74296eb210c11c1092ba06fe1b1625c9e2a8a
Jorge Luis Juarez <jjuarez007@gmail.com>
2013-03-16 17:26:49 -0600

    Okay

--- login.php
+++ login.php
@@ -55,7 +55,7 @@ if(isset($_POST['send'])){
                     })
                 }else{
                     $(".resetPasswd").show("fast",function(){
-                    visiblePass = true;
+                    visiblePass = true; // true
                 });
                 }
         	}
